3b309fce423f50db22e53906504e34184d0f940f
Roan Paulus <79702923+roan-paulus@users.noreply.github.com>
2025-04-06 13:58:42 +0200

    Factor out formatting

---
+++ app/Format.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+
+module Format (formatSearchResult) where
+
+import qualified Ansi (red)
+import Search
+
+-- Take a search result and return a formatted version of the line with the match highlighted within it.
+formatSearchResult :: SearchResult -> String
+formatSearchResult result =
+    show (result.lineNumber + 1) ++ "," ++ show (firstMatch.startIndex + 1) ++ " :: " ++ formattedLine result
+  where
+    firstMatch = head result.matches
+
+formattedLine :: SearchResult -> String
+formattedLine (SearchResult{lineContent, matches = []}) = lineContent
+formattedLine (SearchResult{path, lineContent, lineNumber, matches = (match : remainingMatches)}) =
+    let start = match.startIndex
+        end = match.endIndex
+        beforeMatch = take start lineContent
+        matchedWord = take (end - start + 1) $ drop start lineContent
+        afterMatch = drop (end + 1) lineContent
+        deletedSliceLength = length beforeMatch + length matchedWord
+        offsettedRemaningMatches =
+            map
+                ( \m ->
+                    Slice
+                        { startIndex = m.startIndex - deletedSliceLength
+                        , endIndex = m.endIndex - deletedSliceLength
+                        }
+                )
+                remainingMatches
+     in beforeMatch
+            ++ Ansi.red matchedWord
+            ++ formattedLine
+                SearchResult
+                    { path
+                    , lineContent = afterMatch
+                    , lineNumber = lineNumber -- TODO: Useless data to keep around.
+                    , matches = offsettedRemaningMatches
+                    }
--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,9 @@
 
 module Main where
 
-import qualified Ansi (red)
 import qualified Constants (helpMsg)
 import Control.Exception (SomeException, try)
+import qualified Format
 import Parse (Config (..))
 import qualified Parse
 import Search
@@ -22,45 +22,10 @@ main = do
             let searchResults = concatMap (search config'.pattern) files
             if null searchResults
                 then putStrLn "No matches found."
-                else mapM_ (putStrLn . formatSearchResult) searchResults
+                else mapM_ (putStrLn . Format.formatSearchResult) searchResults
         Nothing -> do
             putStrLn "Config could not be made"
             putStrLn Constants.helpMsg
-
--- Take a search result and return a formatted version of the line with the match highlighted within it.
-formatSearchResult :: SearchResult -> String
-formatSearchResult result =
-    show (result.lineNumber + 1) ++ "," ++ show (firstMatch.startIndex + 1) ++ " :: " ++ formattedLine result
-  where
-    firstMatch = head result.matches
-
-formattedLine :: SearchResult -> String
-formattedLine (SearchResult{lineContent, matches = []}) = lineContent
-formattedLine (SearchResult{path, lineContent, lineNumber, matches = (match : remainingMatches)}) =
-    let start = match.startIndex
-        end = match.endIndex
-        beforeMatch = take start lineContent
-        matchedWord = take (end - start + 1) $ drop start lineContent
-        afterMatch = drop (end + 1) lineContent
-        deletedSliceLength = length beforeMatch + length matchedWord
-        offsettedRemaningMatches =
-            map
-                ( \m ->
-                    Slice
-                        { startIndex = m.startIndex - deletedSliceLength
-                        , endIndex = m.endIndex - deletedSliceLength
-                        }
-                )
-                remainingMatches
-     in beforeMatch
-            ++ Ansi.red matchedWord
-            ++ formattedLine
-                SearchResult
-                    { path
-                    , lineContent = afterMatch
-                    , lineNumber = lineNumber -- TODO: Useless data to keep around.
-                    , matches = offsettedRemaningMatches
-                    }
 
 getFiles :: [FilePath] -> IO [Types.FileBundle]
 getFiles [] = pure []
